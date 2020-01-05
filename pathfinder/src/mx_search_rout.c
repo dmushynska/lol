011b95c9177f8b648298bbe50221131dcb988bab
Dariia Mushynska <dmushynska@e2r4p2.unit.ua>
2020-01-05 20:27:53 +0200

    vlad

--- pathfinder/src/mx_search_rout.c
+++ pathfinder/src/mx_search_rout.c
@@ -1,13 +1,13 @@
 #include "../inc/pathfinder.h"
 
-static void save_result(int *str_result, t_hehx *heh) {
-	heh->str_result = (int *)(malloc)(sizeof(int) * (heh->k));
+// static void save_result(int *str_result, t_hehx *heh) {
+// 	heh->str_result = (int *)(malloc)(sizeof(int) * (heh->k));
 
-	for (int n = 0; n < heh->k; n++)
-		heh->str_result[n] = -1;
-	for (int i = 0; i < heh->k; i++)
-		heh->str_result[i] = str_result[i];
-}
+// 	for (int n = 0; n < heh->k; n++)
+// 		heh->str_result[n] = -1;
+// 	for (int i = 0; i < heh->k; i++)
+// 		heh->str_result[i] = str_result[i];
+// }
 
 static int index_return(int *str_result, int new, t_hehx *heh, int **ser) {
 	int index = 0;
@@ -20,16 +20,16 @@ static int index_return(int *str_result, int new, t_hehx *heh, int **ser) {
 	return index;
 }
 
-static void auditor(int **ser, t_hehx *heh, int* str_result, int h) {
-	while (ser[heh->new_m][h] != -1 && ser[heh->new_m][h] != heh->j) {
-		save_result(str_result, heh);
-		mx_search_rout_2(ser, heh->new_m++, heh, h);
-	}
-	while (ser[heh->new_m][h] >= 0) {
-		save_result(str_result, heh);
-		mx_search_rout_2(ser, heh->new_m++, heh, h);
-	}
-}
+// static void auditor(int **ser, t_hehx *heh, int* str_result, int h) {
+// 	while (ser[heh->new_m][h] != -1 && ser[heh->new_m][h] != heh->j) {
+// 		save_result(str_result, heh);
+// 		mx_search_rout_2(ser, heh->new_m++, heh, h);
+// 	}
+// 	while (ser[heh->new_m][h] >= 0) {
+// 		save_result(str_result, heh);
+// 		mx_search_rout_2(ser, heh->new_m++, heh, h);
+// 	}
+// }
 
 static void search_distance(int **ser, t_hehx *heh, int j) {
 	int h = heh->h;
@@ -38,16 +38,16 @@ static void search_distance(int **ser, t_hehx *heh, int j) {
 	int index = index_return(str_result, new, heh, ser);
 
 	heh->new_arr[heh->lol++] = mx_strdup(heh->arr[h]);
-	auditor(ser, heh, str_result, h);
+	// auditor(ser, heh, str_result, h);
 	while (ser[2][h] != j) {
-			auditor(ser, heh, str_result, h);
+			// auditor(ser, heh, str_result, h);
 		new = ser[0][h];
 		h = ser[2][h];
 		heh->new_arr[heh->lol++] = mx_strdup(heh->arr[h]);
 		new = new - ser[0][h];
 		str_result[index++] = new;
 	}
-	auditor(ser, heh, str_result, h);
+	// auditor(ser, heh, str_result, h);
 	if (ser[0][h] != 0)
 		str_result[index++] = ser[0][h];
 	mx_print_result(heh->k, str_result, ser[0][heh->i], heh);
