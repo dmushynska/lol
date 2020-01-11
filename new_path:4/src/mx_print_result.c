ebd24dd34a748ecb21d7f6a19789a19023976d11
Dariia Mushynska <dmushynska@e2r4p2.unit.ua>
2020-01-11 12:12:05 +0200

    lost

--- new_path:4/src/mx_print_result.c
+++ new_path:4/src/mx_print_result.c
@@ -1,56 +1,68 @@
 #include "pathfinder.h"
 
-// static bool cmp(char **s1, char **s2, char **arr, t_hehx *heh) {
-//     int i = 0;
-//     int j = 0;
-//     int k = 0;
+static bool cmp(char **s1, char **s2, char **arr, t_hehx *heh) {
+    int i = heh->k;
+    int m = heh->k;
+    int j = 0;
+    int k = 0;
 
-//     while (mx_strcmp(s1[i], s2[i]) == 0)
-//         i++;
-//     while (mx_strcmp(s1[i], arr[j]) != 0)
-//         j++;
-//     while (mx_strcmp(s2[i], arr[k]) != 0)
-//         k++;
-//     return j - k;
-// }
+    while (s1[--i] == NULL);
+    while (s2[--m] == NULL);
+    while (mx_strcmp(s1[--i], s2[--m]) == 0);
+    mx_printstr(s1[i]);
+    mx_printstr(s2[m]);
+    while (mx_strcmp(s1[i], arr[j++]) != 0);
+    while (mx_strcmp(s2[m], arr[k++]) != 0);
+    return j - k;
+}
 
-// static void sort_list(t_result **struct_result,
-// bool (*cmp)(char **s1, char **s2, char **arr, t_hehx *heh), t_hehx *heh) {
-//     t_result *i;
-//     t_result *j;
-//     t_result *l;
+static void sort_list(t_result **struct_result,
+bool (*cmp)(char **s1, char **s2, char **arr, t_hehx *heh), t_hehx *heh) {
+    t_result *i;
+    t_result *j;
+    char **new_arr;
 
-//     for (l = *struct_result; l != NULL; l = l->next)
-//         for (i = *struct_result; i != NULL; i = i->next)
-//             for (j = i->next; j != NULL; j = j->next) {
-//                 if ((*cmp)(i->arr_result, j->arr_result, heh->arr, heh) > 0) {
-//                     char **new = i->arr_result;
-//                     // int *new1 = i->result;
-//                     i->arr_result = j->arr_result;
-//                     // i->result = j->result;
-//                     j->arr_result = new;
-//                     // j->result = new1;
-//                     new = NULL;
-//                     // for (int u = 0; u < heh->k; u++)
-//                     //     printf("* %s *", i->arr_result[u]);
-//                     // mx_printchar(10);
-//                 }
-//             }
-// }
+    for (i = *struct_result; i != NULL; i = i->next)
+        for (j = i->next; j != NULL; j = j->next)
+            if ((*cmp)(i->arr_result, j->arr_result, heh->arr, heh) > 0) {
+                mx_copy(i->arr_result, &new_arr, heh);
+                mx_del_strarr(&i->arr_result);
+                mx_copy(j->arr_result, &i->arr_result, heh);
+                mx_del_strarr(&j->arr_result);
+                mx_copy(new_arr, &j->arr_result, heh);
+                mx_del_strarr(&new_arr);
+            }
+}
 
-// static void print_path_rout() {
-//     mx_printstr("Path: ");
-//     mx_printstr(heh->arr[heh->j]);
-//     mx_printstr(" -> ");
-//     mx_printstr(heh->arr[heh->i]);
-//     mx_printstr("\nRoute: ");
-//     while (struct_result->arr_result[--vlad] == NULL);
-//     while (vlad > -1) {
-//         mx_printstr(struct_result->arr_result[vlad--]);
-//         if (vlad >= 0)
-//             mx_printstr(" -> ");
-//         }
-// }
+static void print_path_rout(t_hehx *heh, t_result *struct_result, int vlad) {
+    mx_printstr("Path: ");
+    mx_printstr(heh->arr[heh->j]);
+    mx_printstr(" -> ");
+    mx_printstr(heh->arr[heh->i]);
+    mx_printstr("\nRoute: ");
+    while (struct_result->arr_result[--vlad] == NULL);
+    while (vlad > -1) {
+        mx_printstr(struct_result->arr_result[vlad--]);
+        if (vlad >= 0)
+            mx_printstr(" -> ");
+        }
+}
+
+static void print__distance(t_hehx *heh, t_result *struct_result, int v1,
+int **ser) {
+    mx_printstr("\nDistance: ");
+    while (struct_result->result[--v1] == -1);
+    if (v1 > 1) {
+    while (v1 > -1) {
+            mx_printint(struct_result->result[v1--]);
+            if (v1 >= 0)
+            mx_printstr(" + ");
+        }
+        mx_printstr(" = ");
+    }
+        mx_printint(ser[0][heh->i]);
+        mx_printchar(10);
+}
 
 void mx_print_list(t_result *struct_result, t_hehx *heh, int **ser) {
     int vlad = heh->k + 1;
@@ -59,36 +71,15 @@ void mx_print_list(t_result *struct_result, t_hehx *heh, int **ser) {
 
     // sort_list(&struct_result, cmp, heh);
     while (struct_result != NULL) {
-mx_printstr("========================================\n");
-        // sort_list(&struct_result, cmp, heh);
-        mx_printstr("Path: ");
-        mx_printstr(heh->arr[heh->j]);
-        mx_printstr(" -> ");
-        mx_printstr(heh->arr[heh->i]);
-        mx_printstr("\nRoute: ");
-        while (struct_result->arr_result[--vlad] == NULL);
-        while (vlad > -1) {
-            mx_printstr(struct_result->arr_result[vlad--]);
-            if (vlad >= 0)
-            mx_printstr(" -> ");
-        }
-        mx_printstr("\nDistance: ");
-        while (struct_result->result[--vlad1] == -1);
-        while (vlad1 > -1) {
-            mx_printint(struct_result->result[vlad1--]);
-            if (vlad1 >= 0)
-            mx_printstr(" + ");
-        }
-        mx_printstr(" = ");
-        mx_printint(ser[0][heh->i]);
-        mx_printchar(10);
+        mx_printstr("========================================\n");
+        print_path_rout(heh, struct_result, vlad);
+        print__distance(heh, struct_result, vlad1, ser);
         free(struct_result->result);
         mx_del_strarr(&struct_result->arr_result);
         free(struct_result);
         struct_result = struct_result->next;
         vlad = heh->k + 1;
         vlad1 = heh->k + 1;
-        // mx_printchar('\n');
         mx_printstr("========================================\n");
     }
 }
